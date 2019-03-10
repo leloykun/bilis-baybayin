184482b9196c70b2fe016bfcbbf36013fc114ff3
Franz Louis Cesista <franzlouiscesista@gmail.com>
2019-03-11 06:20:19 +0800

    added data.html

--- website/js/drawer.js
+++ website/js/drawer.js
@@ -1,107 +1,27 @@
-var canvasWidth = 256;
-var canvasHeight = 256;
-var drawing = new Array();
-var paint;
-var d = null;
-var start_time;
-
-var canvasDiv = document.getElementById('canvasDiv');
-var canvas = document.getElementById('canvas');
-if(typeof G_vmlCanvasManager != 'undefined') {
- canvas = G_vmlCanvasManager.initElement(canvas);
-}
-context = canvas.getContext("2d");
-
-function reset_canvas_size() {
-  canvas.setAttribute('width', $(canvasDiv).width()-10);
-  canvas.setAttribute('height', $(canvasDiv).height()-10);
-  redraw();
-}
-reset_canvas_size();
-window.addEventListener("resize", reset_canvas_size);
-
-var getURLParams = function (name) {
-  var params = {};
-  location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (str, key, value) {
-      params[key] = value;
-  });
-
-  return params[name] || params;
-};
-
-var params = getURLParams();
-console.log(params);
-if (params.id) {
-  console.log(params.id);
-  show_drawing(params.id);
+function create_canvas(width, height) {
+  var canv = document.createElement('canvas', '');
+  canv.setAttribute('width', width);
+  canv.setAttribute('height', height);
+  if(typeof G_vmlCanvasManager != 'undefined')
+   canv = G_vmlCanvasManager.initElement(canv);
+  return canv;
 }
 
-var test_ref = firebase.database().ref('drawings');
-test_ref.on('value', got_data, err_data);
-
-function press(e) {
-  var mouseX = (e.changedTouches ? e.changedTouches[0].pageX : e.pageX) - this.offsetLeft;
-  var mouseY = (e.changedTouches ? e.changedTouches[0].pageY : e.pageY) - this.offsetTop;
-
-  d = new Date();
-  if (drawing.length == 0)
-    start_time = d.getTime();
-  time = d.getTime() - start_time;;
-
-  paint = true;
-  addClick(mouseX, mouseY, time, true);
-  redraw();
+function clear_canvas(canv) {
+  var context = canv.getContext("2d");
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 }
 
-function drag(e) {
-  if(paint){
-    var mouseX = (e.changedTouches ? e.changedTouches[0].pageX : e.pageX) - this.offsetLeft;
-    var mouseY = (e.changedTouches ? e.changedTouches[0].pageY : e.pageY) - this.offsetTop;
-
-    d = new Date();
-    time = d.getTime() - start_time;
-
-    addClick(mouseX, mouseY, time, false);
-    redraw();
+function draw_on_canvas(canv, drawing, t_lim, to_scale) {
+  if (to_scale) {
+    var canv_width = canv.clientHeight;
+    var canv_height = canv.clientWidth;
+    drawing = scale_drawing(drawing, canv_width, canv_height);
   }
 
-  // Prevent the whole page from dragging if on mobile
-  e.preventDefault();
-}
+  clear_canvas(canv);
 
-function release() {
-  paint = false;
-  redraw();
-}
-
-function cancel() {
-  paint = false;
-};
-
-// Add mouse event listeners to canvas element
-canvas.addEventListener("mousedown", press, false);
-canvas.addEventListener("mousemove", drag, false);
-canvas.addEventListener("mouseup", release);
-canvas.addEventListener("mouseout", cancel, false);
-
-// Add touch event listeners to canvas element
-canvas.addEventListener("touchstart", press, false);
-canvas.addEventListener("touchmove", drag, false);
-canvas.addEventListener("touchend", release, false);
-canvas.addEventListener("touchcancel", cancel, false);
-
-function addClick(x, y, time, is_new_stroke) {
-  if (is_new_stroke)
-    drawing.push(new Array());
-  drawing[drawing.length-1].push({
-    x: x,
-    y: y,
-    t: time
-  });
-}
-
-function redraw() {
-  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+  var context = canv.getContext("2d");
 
   context.strokeStyle = "black";
   context.lineJoin = "round";
@@ -109,6 +29,8 @@ function redraw() {
 
   for (var i = 0; i < drawing.length; ++i) {
     for (var j = 0; j < drawing[i].length; ++j) {
+      if (drawing[i][j].t > t_lim)
+        break;
       context.beginPath();
       if (j > 0)
         context.moveTo(drawing[i][j-1].x, drawing[i][j-1].y);
@@ -121,18 +43,7 @@ function redraw() {
   }
 }
 
-function clear_canvas() {
-  drawing = new Array();
-  redraw();
-}
-
-function dist_to_line(p, l1, l2) {
-  var u = Math.abs((l2.y - l1.y)*p.x - (l2.x - l1.x)*p.y + l2.x*l1.y - l2.y*l1.x);
-  var v = Math.sqrt(Math.pow(l2.y - l1.y, 2) + Math.pow(l2.x - l1.x, 2));
-  return u/v;
-}
-
-function rescale_drawing() {
+function scale_drawing(drawing, width, height) {
   var minx = Number.POSITIVE_INFINITY;
   var miny = Number.POSITIVE_INFINITY;
 
@@ -149,68 +60,14 @@ function rescale_drawing() {
     }
   }
 
-  var scale = Math.max(maxx - minx, maxy - miny);
+  var scale = Math.min(width, height) / Math.max(maxx - minx, maxy - miny);
 
   for (var i = 0; i < drawing.length; ++i) {
     for (var j = 0; j < drawing[i].length; ++j) {
-      drawing[i][j].x = Math.round((drawing[i][j].x - minx) * (256 / scale));
-      drawing[i][j].y = Math.round((drawing[i][j].y - miny) * (256 / scale));
+      drawing[i][j].x = Math.round((drawing[i][j].x - minx) * scale);
+      drawing[i][j].y = Math.round((drawing[i][j].y - miny) * scale);
     }
-    console.log({origLen: drawing[i].length});
     drawing[i] = simplify(drawing[i], 2, true);
-    console.log({newLen: drawing[i].length});
   }
-}
-
-function save_canvas() {
-  var ref = database.ref('drawings');
-  var data = {
-    character: letra,
-    drawing: drawing
-  }
-  var result = ref.push(data, dataSent);
-  console.log(result.key);
-
-  function dataSent(err, status) {
-    console.log(status);
-  }
-
-  clear_canvas();
-}
-
-function got_data(data) {
-  $('#drawing_list').empty();
-
-  var drawing_list = document.getElementById('drawing_list');
-  if (drawing_list == null)
-    return;
-
-  var drawings = data.val();
-  var keys = Object.keys(drawings);
-  for (var i = 0; i < keys.length; ++i) {
-    var key = keys[i];
-
-    var li = document.createElement('li', '');
-    var perma = document.createElement('a', '');
-    perma.setAttribute('href', '?id=' + key);
-    perma.innerHTML = key;
-    li.appendChild(perma);
-    drawing_list.appendChild(li);
-  }
-}
-
-function err_data(err) {
-  console.log(err);
-}
-
-function show_drawing(key) {
-  var ref = database.ref('drawings/' + key);
-  ref.once('value', one_drawing, err_data);
-
-  function one_drawing(data) {
-    var dbdrawing = data.val();
-    drawing = dbdrawing.drawing;
-    rescale_drawing();
-    redraw();
-  }
+  return drawing;
 }
